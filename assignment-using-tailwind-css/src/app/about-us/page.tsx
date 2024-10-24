0493f38ea702fe6923a7f1e29575c5917658ab54
RIMSHAALEEM <92934551+RIMSHAALEEM@users.noreply.github.com>
2024-10-24 16:27:55 +0500

    Add files via upload

---
+++ assignment-using-tailwind-css/src/app/about-us/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+export default function Aboutus(){
+  return(
+   
+    <div className="parent max-w-full h-screen  bg-white flex">
+     <div className="child pt-20  ml-20">
+       <h1 className="text-5xl  decoration-black  mb-5  font-bold  font-serif">Get To Know Us</h1>
+       <p className="font-serif text-2xl  w-3/4 ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus deserunt nisi exercitationem atque nam a molestias dolorum reiciendis officia voluptate amet obcaecati, numquam, expedita iusto nulla sed aut in. Dolores?</p>
+       <p className="font-serif text-2xl  w-3/4 ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus deserunt nisi exercitationem atque nam a molestias dolorum reiciendis officia voluptate amet obcaecati, numquam, expedita iusto nulla sed aut in. Dolores?</p>
+       </div>  
+        {/* div for imagess */}
+       <div className="images">
+       <img className="img  h-2/4  w-screen float-right mr-40  mt-24 rounded-2xl shadow-2xl " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpIn6BGYr2jGAQ_7XJ4J4dkAcKWU_vm1WAZA&s"/>
+       </div>
+      
+       
+ 
+      
+ 
+       
+ 
+      
+ 
+       
+ </div>
+ 
+   )
+ };
---
+++ assignment-using-tailwind-css/src/components/footer.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function Footer(){
+    return(
+
+    <div className="footer bg-black p-5 text-white  text-center  text-3xl">
+        <h2>Copyright © 2012 - 2024 Rimsha Aleem®. All rights reserved. </h2>
+        
+    </div>
+    )
+};
